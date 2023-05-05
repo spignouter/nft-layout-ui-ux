a97f4f65cdc2456003059165a5842a728c97290e
spignouter <38898870+spignouter@users.noreply.github.com>
2023-05-05 10:23:13 +0800

    feat: урок 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,33 @@
-import * as flsFunctions from "./modules/functions.js";
-import Swiper from 'swiper';
+import * as flsFunctions from './modules/functions.js'
+import Swiper from 'swiper'
 import {
-	Navigation,
-	EffectFade,
-	Pagination,
-	Thumbs,
-	Controller,
-	FreeMode,
-	Scrollbar,
-	A11y,
-	Mousewheel
-} from 'swiper';
-import AOS from 'aos';
+  Navigation,
+  EffectFade,
+  Pagination,
+  Thumbs,
+  Controller,
+  FreeMode,
+  Scrollbar,
+  A11y,
+  Mousewheel,
+} from 'swiper'
+import AOS from 'aos'
 
-flsFunctions.isWebp();
+flsFunctions.isWebp()
+
+// Создание объекта свайпера. принимает строку (css класс) и объект.
+// объект представляет собой набор свойств для свайпера
+const swiper = new Swiper('.auctions-slider', {
+  loop: false, // бесконечный режим
+  navigation: {
+    nextEl: '.swiper-button-next1', // css классы стрелок
+    prevEl: '.swiper-button-next1', // включить стрелочки
+  },
+  modules: [Navigation, EffectFade, Pagination, FreeMode], // модули которые будут использоваться
+  autoHeight: true, // авто высота
+  speed: 500, // скорость прокрутки
+  slidesPerView: 3, // Количество слайдов которые будут видны.
+  spaceBetween: 42,
+  freeMode: true,
+  grabCursor: true,
+})
