84585a5c3e4f71bf5efa44ec22943f90e65a829b
spignouter <38898870+spignouter@users.noreply.github.com>
2023-05-06 17:51:18 +0800

    feat: урок 3

--- src/js/app.js
+++ src/js/app.js
@@ -18,15 +18,45 @@ flsFunctions.isWebp()
 // Создание объекта свайпера. принимает строку (css класс) и объект.
 // объект представляет собой набор свойств для свайпера
 const swiper = new Swiper('.auctions-slider', {
-  loop: false, // бесконечный режим
+  //loop: false, // бесконечный режим
   navigation: {
     nextEl: '.swiper-button-next1', // css классы стрелок
-    prevEl: '.swiper-button-next1', // включить стрелочки
+    prevEl: '.swiper-button-prev1', // включить стрелочки
   },
-  modules: [Navigation, EffectFade, Pagination, FreeMode], // модули которые будут использоваться
+  modules: [Navigation, FreeMode], // модули которые будут использоваться
   autoHeight: true, // авто высота
   speed: 500, // скорость прокрутки
   slidesPerView: 3, // Количество слайдов которые будут видны.
+  spaceBetween: 42,
+  freeMode: true,
+  grabCursor: true,
+})
+
+const swiper2 = new Swiper('.popular-slider', {
+  //loop: false, // бесконечный режим
+  navigation: {
+    nextEl: '.swiper-button-next2', // css классы стрелок
+    prevEl: '.swiper-button-prev2', // включить стрелочки
+  },
+  modules: [Navigation, FreeMode], // модули которые будут использоваться
+  autoHeight: true, // авто высота
+  speed: 500, // скорость прокрутки
+  slidesPerView: 2.5, // Количество слайдов которые будут видны.
+  spaceBetween: 42,
+  freeMode: true,
+  grabCursor: true,
+})
+
+const swiper3 = new Swiper('.categories-slider', {
+  //loop: false, // бесконечный режим
+  navigation: {
+    nextEl: '.swiper-button-next3', // css классы стрелок
+    prevEl: '.swiper-button-prev3', // включить стрелочки
+  },
+  modules: [Navigation, FreeMode], // модули которые будут использоваться
+  autoHeight: true, // авто высота
+  speed: 500, // скорость прокрутки
+  slidesPerView: 2.5, // Количество слайдов которые будут видны.
   spaceBetween: 42,
   freeMode: true,
   grabCursor: true,
